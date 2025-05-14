9b8fdc196fc22b354a9c4364f0c9a07f6a864111
Topu <113087685+topuBhuyan@users.noreply.github.com>
2025-05-14 18:34:31 +0600

    Add files via upload

---
+++ HelloProject.java
@@ -0,0 +1,9 @@
+package myproject.helloproject;
+
+public class HelloProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Topu!");
+        System.out.println("Hello Java!");
+    }
+}
