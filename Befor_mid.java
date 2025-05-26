74d8bae1dd83f425da22b8f3f89f45f30c4f5b11
Topu <113087685+topuBhuyan@users.noreply.github.com>
2025-05-26 06:19:10 +0600

    Add files via upload

---
+++ Befor_mid.java
@@ -0,0 +1,16 @@
+package befor_mid;
+
+
+public class Befor_mid {
+
+    public static void main(String[] args) {
+        
+        Encapluation_test obj = new Encapluation_test();
+        
+        obj.setName("Topu");
+        System.out.println(obj.getName());
+        obj.setAge(21);
+        System.out.println(obj.getAge());
+    }
+}
+
---
+++ Encapluation_test.java
@@ -0,0 +1,21 @@
+package befor_mid;
+
+public class Encapluation_test {
+    private String name;
+    private int age;
+    
+    public void setName(String n) {
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+    
+    public void setAge(int a) {
+        age = a;
+    }
+    public int getAge(){
+        return age;
+    }
+    
+}
